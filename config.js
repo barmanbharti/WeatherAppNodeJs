61915ce70d9b653bc5cf82aca802f94e80404a25
bharti.kumari <bharti.kumari@synoriq.in>
2022-06-23 12:45:34 +0530

    weatherApp commit

---
+++ config.js
@@ -0,0 +1,7 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL:"https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"292943587cd3df3d5b751bcbae30b32c"
+    }
+}
+module.exports = constants;
